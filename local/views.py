d7161aaabe1fe50eb731ea8f92c5fc2922591be5
Krishnadas K.P <krishnadaskp23@gmail.com>
2017-03-11 11:17:04 +0530

    updated views.py
    import itertools was not needed. Also deleted the test comment

--- local/views.py
+++ local/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-import itertools
+
 
 from home.models import FarmerDetails , Local , Price , District , BuyerTransaction , FarmerTransaction ,Stock
 
 
-#This is a test comment for github commit
+
 
 
 
