f28609b24b0c536ba1eda7d78ef2d6cfd0b11b1d
Krishnadas-KP <krishnadaskp23@gmail.com>
2017-03-23 10:25:40 +0530

    production changes

--- Agrocrop/settings.py
+++ Agrocrop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i=8(s%&$*&awv6=36&lm%gv0u#99%-h6@u%1m*y==&-wl@pvah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [u'krishnadas.pythonanywhere.com' , '127.0.0.1']
+ALLOWED_HOSTS = [u'agrocrop.pythonanywhere.com' , '127.0.0.1']
 
 
 # Application definition
